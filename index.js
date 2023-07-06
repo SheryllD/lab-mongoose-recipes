cf55938f1d88a507f764953e044dec3baca696ee
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-07-06 12:45:00 +0200

    save

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const RecipeModel = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// const myRecipe = {
+//name: "Erics home cooking", }; 
 
 // Connection to the database "recipe-app"
 mongoose
@@ -13,11 +15,14 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return RecipeModel.deleteMany();
   })
+  //Iteration 2
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+  return RecipeModel.create
   })
+  
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
